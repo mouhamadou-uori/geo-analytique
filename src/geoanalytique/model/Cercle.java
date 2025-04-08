a986338d3d98a55d9ed200c014a2571d8c261451
aslyyy <adjasuzanely@esp.sn>
2025-04-08 01:10:31 +0000

    compléter la classe cercle

--- src/geoanalytique/model/Cercle.java
+++ src/geoanalytique/model/Cercle.java
@@ -13,15 +13,42 @@ public class Cercle extends Ellipse {
          * Le centre et le rayon sont hérités de la classe Ellipse.
          */
 
-        @Override
-	public <T> T visitor(GeoObjectVisitor<T> obj) throws VisiteurException {
-            // TODO: a completer
-            return null;
-	}
+     /**
+     * Constructeur d’un cercle.
+     * @param centre Centre du cercle
+     * @param rayon Rayon du cercle
+     * @param controleur Contrôleur associé
+     */
+    public Cercle(Point centre, double rayon, GeoAnalytiqueControleur controleur) {
+        super(centre, rayon, rayon, controleur); // rx = ry = rayon
+    }
 
-	@Override
-	public boolean contient(Point p) {
-            // TODO: a completer
-            return false;
-	}
+    /**
+     * Applique un visiteur à ce cercle (pattern Visiteur).
+     *
+     * @param obj Le visiteur
+     * @param <T> Type de retour
+     * @return Résultat du visiteur
+     * @throws VisiteurException En cas d’erreur
+     */
+    @Override
+    public <T> T visitor(GeoObjectVisitor<T> obj) throws VisiteurException {
+        return obj.visitCercle(this);
+    }
+
+    /**
+     * Vérifie si un point est à l’intérieur du cercle.
+     * C’est équivalent au test de l’ellipse avec rx = ry = rayon.
+     *
+     * @param p Le point à tester
+     * @return true si le point est dans le cercle
+     */
+    @Override
+    public boolean contient(Point p) {
+        double dx = p.getX() - getCentre().getX();
+        double dy = p.getY() - getCentre().getY();
+        double distanceCarree = dx * dx + dy * dy;
+        double rayon = getRx(); // ou getRy(), c’est la même chose ici
+        return distanceCarree <= rayon * rayon;
+    }
 }
